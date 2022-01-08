f462207e5c703cbadf2d4a6582ef4d88024f0032
Ivystrode <benjamin200@hotmail.co.uk>
2022-01-08 22:06:32 +0000

    lazypush

--- home_unit/cam.py
+++ home_unit/cam.py
@@ -22,7 +22,7 @@ class Camera():
         
         # trying this
         self.detection_stop = threading.Event()
-        self.object_detection = threading.Thread(target=self.im_recog, args=(self.detection_stop)) # no need to thread - its the only thing the camera will be doing anyway
+        self.object_detection = threading.Thread(target=self.im_recog, args=(1, self.detection_stop)) # no need to thread - its the only thing the camera will be doing anyway
         # YES actually so we can turn it off with a boolean - sort of. It stops the detection so we can use the camera but it leaves the thread running.
         # then we cant restart detection because the thread is alreadyrunning
         
@@ -84,7 +84,7 @@ class Camera():
     def stop_im_recog(self):
         self.detection_stop.set()
     
-    def im_recog(self, detection_stop, counts_before_detect_again=60):
+    def im_recog(self, dontneedthisarg, detection_stop, counts_before_detect_again=60):
         """
         Runs image detection model on the pi, saves pictures of people
         """
