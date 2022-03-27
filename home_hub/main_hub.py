ddea7ade4bde63331b0e2d3f823b227a7e8bad1c
Ivystrode <benjamin200@hotmail.co.uk>
2022-03-27 12:55:34 +0100

    added power saver thread to turn off RF sockets at night

--- home_hub/main_hub.py
+++ home_hub/main_hub.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from tqdm import tqdm
 
 import ntpath, socket, subprocess, threading, time
-import bot, bot_db, wifi
+import bot, bot_db, wifi, rfcon
 
 class HomeHub():
     """
@@ -35,6 +35,9 @@ class HomeHub():
         self.remote_server_listener_thread = threading.Thread(target=self.remote_server_listener, daemon=True)
         self.file_listener_thread = threading.Thread(target=self.file_listener, daemon=True)
         
+        # Electrical socket turn off (save electricity!)
+        self.power_saver_thread = threading.Thread(target=self.power_switch, daemon=True)
+        
 
         
     def activate_hub(self):
@@ -48,7 +51,11 @@ class HomeHub():
         self.unit_listener_thread.start()
         self.file_listener_thread.start()
         self.remote_server_listener_thread.start()
+        
         self.wifi_scanner.scan_thread.start()
+        
+        self.power_saver_thread.start()
+        
         print("Hub active")
         
     def unit_listener(self):
@@ -155,6 +162,54 @@ class HomeHub():
             except Exception as e:
                 print(f"File listener: Receive from local network error: {e}")
                 # s.close()
+                
+    def power_saver(self):
+        """
+        Sends an RF signal that turns off all RF enabled power sockets to save power if they are
+        left on late at night
+        """
+        sent_1 = False
+        sent_2 = False
+        sent_3 = False
+        while True:
+            timenow = datetime.now().strftime("%H%M%S")
+            if (int(timenow) > 235900) and (int(timenow) < 235910) and not sent_1:
+                rfcon.transmit(5, "off") # initially we'll make sure this is the heater...
+                time.sleep(2)
+                rfcon.transmit(5, "off") # send twice to make sure
+                
+                print("[HUB] Sent power saver signal 1")
+                bot.send_message("Power saver signal 1 sent")
+                sent_1 = True
+                
+            # checking an int value on a number that starts with 0 seems to cause issues, so do it as a string
+            if datetime.now().strftime("%H%M") == "0100" and not sent_2:
+                rfcon.transmit(5, "off")
+                time.sleep(2)
+                rfcon.transmit(5, "off")
+                
+                print("[HUB] Sent power saver signal 2")
+                bot.send_message("Power saver signal 2 sent")
+                sent_2 = True
+                
+            if datetime.now().strftime("%H%M") == "0200" and not sent_3:
+                rfcon.transmit(5, "off")
+                time.sleep(2)
+                rfcon.transmit(5, "off")
+                
+                print("[HUB] Sent power saver signal 3")
+                bot.send_message("Power saver signal 3 sent")
+                sent_3 = True
+                
+                
+            if datetime.now().strftime("%H%M") == "0600" and sent_1:
+                # reset signals
+                sent_1 = False
+                sent_2 = False
+                sent_3 = False
+                print("[HUB] Reset power saver booleans")
+                
+            
                 
 
         
