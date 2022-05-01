bbea164a5a4c9b2e402f6f7130e73ca3ca391081
Ivystrode <benjamin200@hotmail.co.uk>
2022-05-01 20:43:23 +0100

    add args to send_command

--- home_hub/commands.py
+++ home_hub/commands.py
@@ -2,14 +2,19 @@ import socket
 import bot_db
 from decouple import config
 
-def send_command(unit, command):
+def send_command(unit, command, *args):
     SEPARATOR = "<SEPARATOR>"
     send_port = int(config("LOCAL_UNIT_RECV_PORT"))
     unit_addr = bot_db.get_unit_address(unit)
     s = socket.socket() 
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.connect((unit_addr, send_port))
-    message = f"{socket.gethostname()}{SEPARATOR}{command}"
+    
+    if not args:
+        message = f"{socket.gethostname()}{SEPARATOR}{command}"
+    else:
+        message = f"{socket.gethostname()}{SEPARATOR}{command}{SEPARATOR}{args[0]}"
+        
     s.send(message.encode())
     print(f"Message sent to {unit}")
     s.close()
