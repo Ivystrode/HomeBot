81e10c0ee91a0c464867775e0e03f7e26967b50c
Ivystrode <benjamin200@hotmail.co.uk>
2022-05-01 21:40:11 +0100

    homebot message tags

--- home_hub/bot.py
+++ home_hub/bot.py
@@ -80,18 +80,18 @@ def plug(update, context):
             if unit[3] == "RF Controller":
                 commands.send_command(unit[1], on_or_off, which_plug)
                 
-                update.message.reply_text(f"OK turned {on_or_off} {which_plug}")
+                update.message.reply_text(f"Homebot: OK turned {on_or_off} {which_plug}")
     except Exception as e:
-        update.message.reply_text(f"Something went wrong - {e}")
+        update.message.reply_text(f"Homebot: Something went wrong - {e}")
         
 def activate_all(update, context):
     try:
         for unit in bot_db.get_all_units():
             if unit[3] == "RF Controller":
                 commands.send_command(unit[1], "activate_all")
-        update.message.reply_text("Sent activation transmissions")
+        update.message.reply_text("Homebot: Sending activation transmissions")
     except Exception as e:
-        update.message.reply_text(f"Something went wrong - {e}")
+        update.message.reply_text(f"Homebot: Something went wrong - {e}")
         
 def deactivate_all(update, context):
     try:
@@ -99,7 +99,7 @@ def deactivate_all(update, context):
             if unit[3] == "RF Controller":
                 commands.send_command(unit[1], "deactivate_all")
     except Exception as e:
-        update.message.reply_text(f"Something went wrong - {e}")
+        update.message.reply_text(f"Homebot: Something went wrong - {e}")
         
 
 def start_object_detection(update, context):
@@ -110,8 +110,8 @@ def start_object_detection(update, context):
                     commands.send_command(unit[1], "start_object_detection")
                     time.sleep(1)
                 except Exception as e:
-                    update.message.reply_text(f"Something went wrong sending to {unit[1]}: {e}")
-            update.message.reply_text("Telling all units to start object detection")
+                    update.message.reply_text(f"Homebot: Something went wrong sending to {unit[1]}: {e}")
+            update.message.reply_text("Homebot: Telling all units to start object detection")
     except Exception as e:
         update.message.reply_text("Something went wrong")
         update.message.reply_text(f"{e}")
@@ -121,29 +121,29 @@ def stop_object_detection(update, context):
         if len(context.args) == 0:
             for unit in bot_db.get_all_units():
                 commands.send_command(unit[1], "stop_object_detection")
-            update.message.reply_text("Telling all units to stop object detection")
+            update.message.reply_text("Homebot: Telling all units to stop object detection")
     except Exception as e:
-        update.message.reply_text("Something went wrong")
+        update.message.reply_text("Homebot: Something went wrong")
         update.message.reply_text(f"{e}")
         
 def start_wifi_scan(update, context):
-    update.message.reply_text("Starting the wifi scanner...")
+    update.message.reply_text("Homebot: Starting the wifi scanner...")
 
 def stop_wifi_scan(update, context):
-    update.message.reply_text("Stopping the wifi scanner...")
+    update.message.reply_text("Homebot: Stopping the wifi scanner...")
         
 def send_pic(update, context):
     try:
         if len(context.args) == 0:
             for unit in bot_db.get_all_units():
                 commands.send_command(unit[1], "send_photo")
-            update.message.reply_text("Getting picture from all units")
+            update.message.reply_text("Homebot: Getting picture from all units")
         else:
             for unit in context.args:
                 commands.send_command(unit, "send_photo")
-                update.message.reply_text(f"Getting picture from {unit}...")
+                update.message.reply_text(f"Homebot: Getting picture from {unit}...")
     except Exception as e:
-        update.message.reply_text("Something went wrong")
+        update.message.reply_text("Homebot: Something went wrong")
         update.message.reply_text(f"{e}")
  
 def reboot(update, context):
@@ -151,13 +151,13 @@ def reboot(update, context):
         if len(context.args) == 0:
             for unit in bot_db.get_all_units():
                 commands.send_command(unit[1], "reboot")
-            update.message.reply_text("Rebooting all units")
+            update.message.reply_text("Homebot: Rebooting all units")
         else:
             for unit in context.args:
                 commands.send_command(unit, "reboot")
-                update.message.reply_text(f"Rebooting {unit}...")
+                update.message.reply_text(f"Homebot: Rebooting {unit}...")
     except Exception as e:
-        update.message.reply_text("Something went wrong")
+        update.message.reply_text("Homebot: Something went wrong")
         update.message.reply_text(f"{e}")
 
 def ip_monitor():
@@ -180,7 +180,7 @@ def ip_monitor():
             # ===SEND WARNING MESSAGE===
             for user in bot_db.get_all_users():
                 try:
-                    updater.bot.sendMessage(user[0], f"IP Address updated, old address:\n{stored_ip}\nNew address:\n{ip}", timeout=50)
+                    updater.bot.sendMessage(user[0], f"Homebot: IP Address updated, old address:\n{stored_ip}\nNew address:\n{ip}", timeout=50)
                 except Exception as e:
                     print(e)
             
